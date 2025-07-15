73a96e781b29a06e2e89da9bf56251d9d84f9673
LiuAlexR <alexliu3000@gmail.com>
2025-07-15 12:08:46 -0600

    Add video

--- src/react-app/pages/homepage/Centerpiece.tsx
+++ src/react-app/pages/homepage/Centerpiece.tsx
@@ -1,12 +1,13 @@
 import logo from "../../assets/logos/aeonix.png";
 //import DroneVideo from "../../assets/media/homepage/drone_video.mp4";
-import DroneImage from "../../assets/media/homepage/drone-watering-fields-2.png"
+// import DroneImage from "../../assets/media/homepage/drone-watering-fields-2.png"
+import DroneVideo from "../../assets/media/homepage/the-video.mp4";
 import "./Centerpiece.css";
 export default function Centerpiece() {
     return (
         <div id="centerpiece">
-            {/* <video muted loop autoPlay id="background_video"><source src={DroneVideo} type="video/mp4" /></video> */}
-            <img src={DroneImage} id="background_video"></img>
+            <video muted loop autoPlay id="background_video"><source src={DroneVideo} type="video/mp4" /></video>
+            {/* <img src={DroneImage} id="background_video"></img> */}
             <div id="video_overlay"></div>
             <div id="center_text">
                 <h1>
